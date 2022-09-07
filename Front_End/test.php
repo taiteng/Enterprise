8a5cdb01ef659e37b1a2cd6b6d49f83186008a96
taite <taiengchan@gmail.com>
2022-09-07 21:17:59 +0800

    test

--- Front_End/test.php
+++ Front_End/test.php
@@ -1,7 +1,72 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <!-- Bootstrap plugin -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        
+        <!-- logo -->
+        <link rel="icon" href="">
+        
+        <style>
+            body {background-color: black;}
+            h1   {color: white;
+                  text-align: center;}
+            p    {color: white;}
+            a    {color: white;
+                  text-decoration: none;}
+            
+            /* width */
+            ::-webkit-scrollbar {
+              width: 10px;
+            }
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+            /* Track */
+            ::-webkit-scrollbar-track {
+              background: #f1f1f1;
+            }
 
+            /* Handle */
+            ::-webkit-scrollbar-thumb {
+              background: #888;
+            }
+
+            /* Handle on hover */
+            ::-webkit-scrollbar-thumb:hover {
+              background: #555;
+            }
+
+        </style>
+        
+        <title>Test</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-sm bg-white navbar-light fixed-top">
+                <div class="container-fluid">
+                    <a class="navbar-brand">
+                        <img src="" alt="logo" onclick="location.href='home.php'"/>
+                    </a>
+                    <div class="d-flex flex-row bd-highlight mb-3 justify-content-end">
+                        <ul class="navbar-nav nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="home.php" style="font-size: 20px;">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="" style="font-size: 20px;">Policy</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="" style="font-size: 20px;">Help</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="" style="font-size: 20px;">Profile</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </body>
+</html>
