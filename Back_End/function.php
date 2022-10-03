2b96bed9351e449e126fa2b7575392f9ed0017c3
taite <taiengchan@gmail.com>
2022-10-04 01:04:27 +0800

    JavaScript for displaying selection based on database is done, enhanced quotation UI

--- Back_End/function.php
+++ Back_End/function.php
@@ -1,17 +1,38 @@
 <?php
 session_start();
 
-function random_id_gen($length)
-    {
-        //the characters you want in your id
-        $characters = '123456789ABCDEFGHJKLMNPQRSTUVWXYZ';
-        $max = strlen($characters) - 1;
-        $string = '';
+function random_id_gen($length){
+    //the characters you want in your id
+    $characters = '123456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+    $max = strlen($characters) - 1;
+    $string = '';
 
-        for ($i = 0; $i < $length; $i++) {
-            $string .= $characters[mt_rand(0, $max)];
+    for ($i = 0; $i < $length; $i++) {
+        $string .= $characters[mt_rand(0, $max)];
+    }
+
+    $_SESSION['sid'] = $string;
+    return $string;
+}
+
+function check_service($conn){
+    if(isset($_SESSION['sid'])){
+	$sid = $_SESSION['sid'];
+	$query = "select * from service where service_id = '$sid'";
+
+	$result = mysqli_query($conn,$query);
+        if($result && mysqli_num_rows($result) > 0)
+        {
+            $service_data = mysqli_fetch_assoc($result);
+            return $service_data;
         }
 
-        $_SESSION['sid'] = $string;
-        return $string;
+        header("Location: ../Front_End/quotation.php");
+        die;
     }
+    else{
+        //redirect to login
+        header("Location: ../Front_End/home.php");
+        die;
+    }
+}
