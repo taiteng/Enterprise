a53e1597c7918ec35f870bc531c379692d0fa635
taite <taiengchan@gmail.com>
2022-10-21 16:27:26 +0800

    bug settled

--- Back_End/quotation_reject.php
+++ Back_End/quotation_reject.php
@@ -1,13 +1,8 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-// required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  
 // include database and object files
 include_once 'Service_API/database.php';
 include_once 'Service_API/service.php';
